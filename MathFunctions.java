63c69f9f2de86f47d8f7570da9938cb1ede0c5d4
mihirp23 <mihirp38661@gmail.com>
2017-10-20 08:13:43 -0400

    Added getExponent and getSquareRoot

--- MathFunctions.java
+++ MathFunctions.java
@@ -1,6 +1,7 @@
-package functions;
 
-import java.math.BigDecimal;
+// Mihir - 10/20/17
+// Importing the entire math package
+import java.math.*;
 
 public class MathFunctions {
 	
@@ -8,13 +9,13 @@ public class MathFunctions {
 	public static BigDecimal getRemaineder(BigDecimal bd1, BigDecimal bd2) {
 		return bd1.remainder(bd2);
 	}
-	
-	
+		
+		
 	//This function pulls the full answer of the first parameter divided by the second
 	public static BigDecimal Div(BigDecimal bd1, BigDecimal bd2) {
 		return bd1.divide(bd2);
 	}
-	
+		
 	//Decimal To Binary
 	public static String decToBin(BigDecimal bd1) {
 		if (!Integer.toString(bd1.intValue()).equals(bd1.toString())){
@@ -24,7 +25,25 @@ public class MathFunctions {
 		return Integer.toBinaryString(bd1.intValue());
 	}
 	
-	public static void main(String args[]) {
+	/* ----------------------- getExponent() ----------------------
+	 * Mihir - 10/20/17
+	 * returns the value of the first provided value raised to the value 
+	 * of the second provided value.
+	*/
+	public static BigDecimal getExponent (BigDecimal d, int n) {
+		return (d.pow(n));
+	}
+
+	/* ----------------------- getSquareRoot() ----------------------
+	 * Mihir - 10/20/17
+	 * returns the square root value of the provided number
+	*/
+	public static double getSquareRoot(double d1) {
+		return (Math.sqrt(d1));
+	}
+	
+	/* ----------------------- main() ---------------------- */
+	public static void main(String[] args) {
 		BigDecimal b = new BigDecimal("5");
 		BigDecimal a = new BigDecimal("2");
 		BigDecimal c = new BigDecimal("2.6");
@@ -38,7 +57,18 @@ public class MathFunctions {
 		System.out.println(MathFunctions.getRemaineder(c, b));
 		System.out.println(MathFunctions.decToBin(d));
 		
+		// Mihir - 10/20/17
+		// test for getExponent()
+		BigDecimal x = new BigDecimal("3");
+		int n = 2;
+		BigDecimal exponent_value = getExponent(x,n);
+		System.out.println(x + " raised to the power of " + n + " is " + exponent_value);
+		
+		// test for getSquareRoot() 
+		double s = 4;
+		double square_root_value = getSquareRoot(s);
+		System.out.println("Sqrt of " + s + " is " + square_root_value);
 	}
 	
-	
+
 }
